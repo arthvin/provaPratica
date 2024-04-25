b273c8f38894f48c6d45881ce30f76f2392babf8
Arthur Semensati <112193063+arthvin@users.noreply.github.com>
2024-04-25 00:20:43 +0000

    comitt

--- index.js
+++ index.js
@@ -1,33 +1,38 @@
-async function getPokemonData(UF) {
+async function geteEstadoData(codigoEstados) {
     try {
-      const data = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${UF}`);
+      const data = await fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${codigoEstados}/municipios`);
       const jsonData = await data.json();
   
       const spritesArray = Object.values(jsonData.sprites).filter(sprite => typeof sprite === 'string');
-      generateInfoSection(spritesArray, UF);
+      generateInfoSection(spritesArray, codigoEstados);
     } catch (error) {
       console.error(error);
     }
   }
 
-  function generateInfoSection(sprites, UFname) {
+  function getSearchParams() {
+  
+    if (!location.search) {
+      return;
+    }
+  
+    const urlSearchParams = new URLSearchParams(location.search);
+  
+    const codigoEstados = urlSearchParams.get('uf');
+    
+    changePageTitle(`${UF}`);
+    getEstadoData(UF);
+  }
+
+  function changePageTitle(title) {
+    document.title = title;
+  }
+  
+  function generateInfoSection(sprites, estadosNome) {
     const h2 = document.createElement('h2');
-    h2.id = "info-municipio-label";
-    h2.textContent = `Informações sobre ${UFname}`;
+    h2.id = "info-estados-label";
+    h2.textContent = `Informações sobre ${estadosNome}`;
 
     const h1 = document.createElement('h1');
     h1.id = "page-title";
-    h1.textContent = `Página do ${UFname}`;
-  
-    const img = document.createElement('img');
-    //const img = document.querySelector('img');
-    img.src = sprites[0];
-    img.alt = `Imagem do pokemon ${UFname}`;
-    let currentIndex = 0;
-  
-    img.addEventListener('click', () => {
-      currentIndex = (currentIndex + 1) % sprites.length;
-      img.src = sprites[currentIndex];
-    });
-
-  }
+    h1.textContent = `${estadosNome}`;
